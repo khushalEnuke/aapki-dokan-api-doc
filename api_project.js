0f14d28e8290895b7abac205d96b80c31585d697
Khushal <khushal.pahuja@enukesoftware.com>
2019-05-16 12:30:35 +0530

    initial commit

---
+++ api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "Aapki Dokan",
+  "version": "0.1.0",
+  "description": "Aapki dokan Api Documentation",
+  "title": "Aapki Dokan API",
+  "url": "http://aapkidokan.yiipro.com",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-05-16T06:55:04.234Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
